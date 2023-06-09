310f11a9fe33a3d7100414c5a0af4641b094f630
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-09 16:13:04 +0500

    insert multiple users from randomuser

--- api.py
+++ api.py
@@ -1,3 +1,23 @@
 import requests
 
 base_url = "https://randomuser.me/api/"
+
+def get_users(n: int) -> list:
+    payload = {
+        "results": n
+    }
+    response = requests.get(base_url, params=payload)
+
+    users = []
+    for user in response.json()['results']:
+        user_data = {
+            "first_name": user['name']['first'],
+            "last_name": user['name']['last'],
+            "age": user['dob']['age'],
+            "phone": user['phone'],
+            "country": user['location']['country'],
+            "email": user['email'],
+        }
+        users.append(user_data)
+
+    return users
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from tinydb import TinyDB
+from api import get_users
 
-db = TinyDB('data.json')
+db = TinyDB('data.json', indent=4)
+
+users = get_users(30)
+db.insert_multiple(users)
